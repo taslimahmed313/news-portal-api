a17759227553dbea7ba1f87c6833c6a26165dbb3
Taslim <taslimahmed313@gmil.com>
2022-09-03 00:45:09 +0600

    News Details

--- js/main.js
+++ js/main.js
@@ -1,20 +1,61 @@
-const loadNewsCategories = () =>{
-    const url = `https://openapi.programming-hero.com/api/news/categories`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => displayNewsCategories(data.data.news_category));
-}
-const displayNewsCategories = newses =>{
-    console.log(newses);
-    const newsContainer = document.getElementById("news-container");
-    newses.forEach(news => {
-        console.log(news)
-        const newsSpan = document.createElement('span');
-        newsSpan.classList.add('mx-3')
-        newsSpan.innerHTML = `
-            <span>${news.category_name}</span>
+const loadNewsCategories = () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayNewsCategories(data.data.news_category));
+};
+const displayNewsCategories = (newses) => {
+  console.log(newses);
+  const newsContainer = document.getElementById("news-container");
+  newses.forEach((news) => {
+    console.log(news);
+    const newsSpan = document.createElement("span");
+    newsSpan.classList.add("mx-3");
+    newsSpan.innerHTML = `
+            <span onclick = "loadNewsDetails('${news.category_id}')">${news.category_name}</span>
         `;
-        newsContainer.appendChild(newsSpan);
-    });
-}
+    newsContainer.appendChild(newsSpan);
+  });
+};
+const loadNewsDetails = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayNewsDetails(data.data));
+};
+const displayNewsDetails = (newses) => {
+  console.log(newses);
+  const newsDetailsContainer = document.getElementById(
+    "news-details-container"
+  );
+  newsDetailsContainer.textContent = "";
+  newses.forEach((news) => {
+    console.log(news);
+    const newsDiv = document.createElement("div");
+    newsDiv.classList.add("card", "mb-3");
+    newsDiv.innerHTML = `
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${news.thumbnail_url}" class="img-fluid w-100 height-control rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${news.title}</h5>
+        <p class="card-text">${news.details}</p>
+        <div class="display-flex">
+        <div>
+        <img class="img-thumbnail rounded-5 img-fluid size" src = "${news.author.img}">
+        </div>
+        <div class="ms-2">
+        <p class="mb-0">${news.author.name}</p>
+        <p class="mb-0">${news.author.published_date}</p></div>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+    newsDetailsContainer.appendChild(newsDiv);
+  });
+};
+
 loadNewsCategories();
