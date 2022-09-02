1a43bc1e35309987faba4cb533da63c0ec7d0e23
Taslim <taslimahmed313@gmil.com>
2022-09-02 23:08:53 +0600

    News Categories Add

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log('It is Connected........')
+const loadNewsCategories = () =>{
+    const url = `https://openapi.programming-hero.com/api/news/categories`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => displayNewsCategories(data.data.news_category));
+}
+const displayNewsCategories = newses =>{
+    console.log(newses);
+    const newsContainer = document.getElementById("news-container");
+    newses.forEach(news => {
+        console.log(news)
+        const newsSpan = document.createElement('span');
+        newsSpan.classList.add('mx-3')
+        newsSpan.innerHTML = `
+            <span>${news.category_name}</span>
+        `;
+        newsContainer.appendChild(newsSpan);
+    });
+}
+loadNewsCategories();
