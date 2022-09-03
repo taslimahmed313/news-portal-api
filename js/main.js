eee363ba2d847883efb49722bd67d073b794b277
Taslim <taslimahmed313@gmil.com>
2022-09-03 06:24:21 +0600

    Image sizing

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+// Load News Categories.......................................................
 const loadNewsCategories = () => {
   const url = `https://openapi.programming-hero.com/api/news/categories`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayNewsCategories(data.data.news_category));
 };
+
+// Display News Categories......................................................
 const displayNewsCategories = (newses) => {
   console.log(newses);
   const newsContainer = document.getElementById("news-container");
@@ -17,36 +20,44 @@ const displayNewsCategories = (newses) => {
     newsContainer.appendChild(newsSpan);
   });
 };
+
+// Load News Details.............................................................
 const loadNewsDetails = (id) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayNewsDetails(data.data));
 };
+
+// Display News Details..........................................................
 const displayNewsDetails = (newses) => {
   console.log(newses);
-  const newsDetailsContainer = document.getElementById(
-    "news-details-container"
-  );
+
+  //   'No News found' -message...............................................
   const newsMessage = document.getElementById("no-news-message");
   if (newses.length === 0) {
     newsMessage.classList.remove("d-none");
   } else {
     newsMessage.classList.add("d-none");
   }
+
+  //   Finds and Sets a Single news
+  const newsDetailsContainer = document.getElementById(
+    "news-details-container"
+  );
   newsDetailsContainer.textContent = "";
   newses.forEach((news) => {
     console.log(news);
     const newsDiv = document.createElement("div");
-    newsDiv.classList.add("card", "mb-3");
+    newsDiv.classList.add("card", "mb-3", "px-2");
     newsDiv.innerHTML = `
-  <div class="row g-0">
-    <div class="col-md-4">
+  <div class="row g-0 d-flex align-items-center">
+    <div class="col-md-3 col-4">
       <img src="${
         news.thumbnail_url
       }" class="img-fluid w-100 height-control rounded-start" alt="...">
     </div>
-    <div class="col-md-8">
+    <div class="col-md-9 col-8">
       <div class="card-body">
         <h5 class="card-title">${news.title}</h5>
         <p class="card-text">${news.details.slice(0, 250)}...</p>
@@ -54,13 +65,15 @@ const displayNewsDetails = (newses) => {
         <div>
         <div class="d-flex align-items-center">
         <div>
-        <img class="img-thumbnail rounded-5 img-fluid size" src = "${
+        <img class="img-thumbnail rounded-5 img-fluid img-size" src = "${
           news.author.img
         }">
         </div>
         <div class="ms-2">
-        <p class="mb-0">${news.author.name}</p>
-        <p class="mb-0">${news.author.published_date.slice(0,11)}</p></div>
+        <p class="mb-0">${
+          news.author.name ? news.author.name : "Mr. No Data"
+        }</p>
+        <p class="mb-0">${news.author.published_date.slice(0, 11)}</p></div>
         </div>
         </div> 
 
@@ -75,8 +88,9 @@ const displayNewsDetails = (newses) => {
       <i class="fa-regular fa-star"></i>
       </div>
       <div>
-      <i class="fa-solid fa-arrow-right" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick = "loadNewsDetailsModal('${
-        news._id}')"></i>
+      <i class="fa-solid fa-arrow-right" data-bs-toggle="modal" data-bs-target="#newsModal" onclick = "loadNewsDetailsModal('${
+        news._id
+      }')"></i>
       </div>
       
       </div>
@@ -89,23 +103,24 @@ const displayNewsDetails = (newses) => {
   });
 };
 
+// Load Modal News..............................................................
+const loadNewsDetailsModal = (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/${id}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayModalNewsDetails(data.data));
+};
 
-const loadNewsDetailsModal = (id) =>{
-    const url = `https://openapi.programming-hero.com/api/news/${id}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => displayModalNewsDetails(data.data));
-}
-
-const displayModalNewsDetails = newses =>{
-    // console.log(newses);
-    newses.forEach(news =>{
-        console.log(news);
-        const newsModalTitle = document.getElementById("exampleModalLabel");
-        newsModalTitle.innerText = `${news.title}`;
-        const newsModalDetails = document.getElementById("news-details-modal");
-        newsModalDetails.innerText = `${news.details}`;
-    })
-}
+// Display Modal News...........................................................
+const displayModalNewsDetails = (newses) => {
+  // console.log(newses);
+  newses.forEach((news) => {
+    console.log(news);
+    const newsModalTitle = document.getElementById("newsModalLabel");
+    newsModalTitle.innerText = `${news.title}`;
+    const newsModalDetails = document.getElementById("news-details-modal");
+    newsModalDetails.innerText = `${news.details}`;
+  });
+};
 // loadNewsDetailsModal();
 loadNewsCategories();
